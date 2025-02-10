0147f258a65b8e072277ed2c575546c895600b79
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2025-02-10 12:21:40 +0700

    feat: create auto summairzation to reduce token usage, create reset keyword and recreate first instruction

--- src/lib/groq.ts
+++ src/lib/groq.ts
@@ -1,20 +1,31 @@
 import { env } from "@/env"
 import Groq from "groq-sdk"
-import { Model } from "groq-sdk/resources/models.mjs"
+import type {
+  ModelListResponse as GroqModelListResponse,
+  Model,
+} from "groq-sdk/resources/models.mjs"
 
 import { Config } from "./config"
+
+export interface ModelListResponse extends GroqModelListResponse {
+  data: Array<
+    Model & {
+      context_window: number
+    }
+  >
+}
 
 const groq = new Groq({
   apiKey: env.GROQ_API_KEY,
 })
 
 export const getGroqModelList = async () => {
-  const models = await groq.models.list()
+  const models = (await groq.models.list()) as unknown as ModelListResponse
   return models
 }
 
 export let GROQ_MODEL_LIST: Model["id"][] = []
-export let GROQ_CURRENT_MODEL = "llama-3.3-70b-versatile"
+export let GROQ_CURRENT_MODEL = Config.GROQ.DEFAULT_MODEL
 
 export const setCurrentGroqModel = (model: Model["id"]) => {
   GROQ_CURRENT_MODEL = model
@@ -26,33 +37,34 @@ export const getGroqChatCompletion = async (
   return groq.chat.completions.create({
     messages: [
       {
+        name: "instruction",
         role: "system",
         content: `${Config.GROQ.INSTRUCTION}. Model yang kamu gunakan saat ini adalah ${GROQ_CURRENT_MODEL}.`,
       },
       ...history,
     ],
     model: GROQ_CURRENT_MODEL,
-
-    // Controls randomness: lowering results in less random completions.
-    // As the temperature approaches zero, the model will become deterministic
-    // and repetitive.
     temperature: 1.2,
-
-    // The maximum number of tokens to generate. Requests can use up to
-    // 2048 tokens shared between prompt and completion.
-    max_completion_tokens: 1024,
-
-    // Controls diversity via nucleus sampling: 0.5 means half of all
-    // likelihood-weighted options are considered.
     top_p: 1,
-
-    // A stop sequence is a predefined or user-specified text string that
-    // signals an AI to stop generating content, ensuring its responses
-    // remain focused and concise. Examples include punctuation marks and
-    // markers like "[end]".
     stop: null,
-
-    // If set, partial message deltas will be sent.
+    stream: false,
+  })
+}
+export const getGroqChatSummary = async (
+  history: Groq.Chat.Completions.ChatCompletionMessageParam[]
+) => {
+  const historyJson = JSON.stringify(history)
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `Please provide a concise summary of the following conversation between you (assistant) and user. History: ${historyJson}`,
+      },
+    ],
+    model: GROQ_CURRENT_MODEL,
+    temperature: 0.3,
+    top_p: 1,
+    stop: null,
     stream: false,
   })
 }
